9d5979649dedbcea364cccb36bbf6d0a75cc64c5
Garret Beebe <garret@TK-421-MAC.local>
2019-07-29 14:52:18 -0700

    initial commit

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key: 'PeTMQrGotbmQU6HKYiXHBGGrx',
+  consumer_secret: 'LFqr7Ed4Brt3sDzOVPg8xhHB3rA8gOnG0M1jD9phv8PiBqcGgE',
+  access_token_key: '16180037-cX3xmKs1Xrs5FElSYoRIOlYjFwULmUkqQYOrlgW3p',
+  access_token_secret: 'BoT7gSap6Y33yv5LIlisHCtTOhAzIuDaOhw7YSefLZ5zP'
+}
